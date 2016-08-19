6aadbac3427c658daf350d978d7efb72d9eaa0b0
Harsha <harsha.srinivasa@intellicar.in>
2016-08-19 13:29:39 +0530

    management nav bar

--- src/app/components/common/constants.js
+++ src/app/components/common/constants.js
@@ -10,10 +10,12 @@
     function constantsDashboard() {
         var STATE_HOME = "home";
         var STATE_HOME_DASHBOARD = "home.dashboard";
+        var STATE_HOME_MANAGEMENT = "home.management"
 
         return {
             STATE_HOME : STATE_HOME,
-            STATE_HOME_DASHBOARD : STATE_HOME_DASHBOARD
+            STATE_HOME_DASHBOARD : STATE_HOME_DASHBOARD,
+            STATE_HOME_MANAGEMENT : STATE_HOME_MANAGEMENT
         }
     }
 
--- src/app/components/landingpage/management/management.controller.js
+++ src/app/components/landingpage/management/management.controller.js
@@ -8,11 +8,16 @@
         .module('uiplatform')
         .controller('ManagementController', ManagementController);
 
-    function ManagementController($scope, $rootScope, navService, $mdSidenav, $log, $document, requestService) {
+    function ManagementController($scope, $state,$rootScope, navService, $mdSidenav,
+                                  $log, $document, requestService) {
 
         $log.log('ManagementController');
         var vm = this;
 
+        vm.toggleLeftnav = function(event, data) {
+            $log.log('management navvvvvvvv ')
+            vm.left_nav_toggle = data.left_nav_toggle;
+        }
 
     }
 })();
