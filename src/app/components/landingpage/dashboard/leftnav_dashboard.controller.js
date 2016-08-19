60304ff0143d5af6799ef6e2d5e1385efb041d7a
Harsha <harsha.srinivasa@intellicar.in>
2016-08-19 13:31:06 +0530

    merge conflict

--- src/app/components/landingpage/dashboard/leftnav_dashboard.controller.js
+++ src/app/components/landingpage/dashboard/leftnav_dashboard.controller.js
@@ -16,20 +16,18 @@
         vm.state = $state;
 
         vm.toggleLeftnav = function(event, data) {
-
-            //$log.log('dashboard navvvvvvvv ')
-            vm.dummy = data.dummy;
+           // $log.log('dashboard navvvvvvvv ')
+            vm.left_nav_toggle = data.left_nav_toggle;
         }
 
         vm.displayTree = function(data) {
             //$log.log("displayTree");
            // $log.log(data);
-            $scope.tree_data = treeDataService.arrange_data(data);
+           // $scope.tree_data = treeDataService.dashboard_tree_data(data);
         }
 
-
         vm.initialize = function() {
-            console.log(vm.state);
+           // console.log(vm.state);
             if(vm.state.current.name == constantsDashboard.STATE_HOME_DASHBOARD ||
                 vm.state.current.name == constantsDashboard.STATE_HOME) {
                 leftNavDashboardService.getTree();
@@ -39,60 +37,5 @@
         leftNavDashboardService.addTreeCallback(vm.displayTree);
         vm.initialize();
         $scope.$on('toggleLeftnav', vm.toggleLeftnav);
-
-/*
-        $scope.tree_data = [
-            {
-                "id": 1,
-                "title": "node1",
-                "nodes": [
-                    {
-                        "id": 22,
-                        "title": "node1.1",
-                        "nodes": [
-                            {
-                                "id": 111,
-                                "title": "node1.1.1",
-                                "nodes": []
-                            }
-                        ]
-                    },
-                    {
-                        "id": 12,
-                        "title": "node1.2",
-                        "nodes": []
-                    }
-                ]
-            },
-            {
-                "id": 7,
-                "title": "node2",
-                "nodrop": true,
-                "nodes": [
-                    {
-                        "id": 71,
-                        "title": "node2.1",
-                        "nodes": []
-                    },
-                    {
-                        "id": 22,
-                        "title": "node2.2",
-                        "nodes": []
-                    }
-                ]
-            },
-            {
-                "id": 3,
-                "title": "node3",
-                "nodes": [
-                    {
-                        "id": 31,
-                        "title": "node3.1",
-                        "nodes": []
-                    }
-                ]
-            }
-        ];
-        */
     }
 })();
