238dc66b22e0af5ae9563ac14ddd3a0c2ae63ead
Harsha <harsha.srinivasa@intellicar.in>
2016-08-23 17:12:54 +0530

    Tab click event issue fixed and added logo in login page

--- src/app/components/landingpage/management/settings/settings.controller.js
+++ src/app/components/landingpage/management/settings/settings.controller.js
@@ -7,7 +7,6 @@
     angular
         .module('uiplatform')
         .controller('SettingsController', SettingsController);
-
     function SettingsController($scope, $rootScope, $log, $state) {
 
         $log.log('SettingsController');
@@ -15,6 +14,8 @@
 
         // $log.log('state.name = ');
         // $log.log($state);
+
+
     }
 })();
 
