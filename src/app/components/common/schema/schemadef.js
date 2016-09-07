71db5f01a1cd21fea81e9d9312b1df48fd51bedc
smiddela <smiddela@intellicar.in>
2016-09-07 20:49:13 +0530

    Add schema file for settings

--- src/app/components/common/schema/schemadef.js
+++ src/app/components/common/schema/schemadef.js
@@ -7,28 +7,84 @@
 var response = [
     ["perm1", "SETTINGS_TAG_1", {
         // output => <input type="number">
-        "key1": {"type": ["int"], "displayname": "Display Name1", "displaydesc":"This will come in the hint", 
-        "select": [0, 1, [1,3,9,27,81]], "default":[3], "editable":true},
-        
-        "keyobj": {"type":[{"level2key1":{"type":"int", "displayname":"Level 2 Key", "default":10, "editable":true}}],
-        "displayname":"Level 2 Keys Array", "default":[{"level2key1":100}, {"level2key1":200}], "checkfun":{"type":"udf",
-        "func":'function(inpdataofthisfield, overalldataofthisjson, schemaforthisfield, overallschema){
-            return {"status":"FAILURE", "errmsg":"I haven't yet checked it"};}'}},
+        "key1": {"type": "int", "displayname": "Display Name1", "displaydesc": "This will come in the hint",
+            "select": null, "editable": true, "default":[null]},
+
 
         // output => <input type="text">
-        "key2": {"type": "string", "displayname": "Display Name2", "select": null},
+        "key2": {"type": "string", "displayname": "Display Name2", "displaydesc": "This will come in the hint",
+            "select": null, "editable": true, "default":[null]},
+
+
+        // output => List of integers in items (Read only)
+        "key3": {"type": ["int"], "displayname": "Display Name3", "displaydesc": "This will come in the hint",
+            "select": [-1, -1, [1, 2, 3, 4, 5]], "editable": false, "default":[null]},
+
+
+        // output => List of strings in items (Read only)
+        "key4": {"type": int["string"], "displayname": "Display Name4", "displaydesc": "This will come in the hint",
+            "select": [-1, -1, ['a', 'b', 'c', 'd']], "editable": false, "default":[null]},
+
 
         // output => List of integers in items with Add button
-        "key3": {"type": "[int]", "displayname": "Display Name3", "select": false, "items": [1, 2, 3, 4]},
+        "key5": {"type": ["int"], "displayname": "Display Name5", "displaydesc": "This will come in the hint",
+            "select": [-1, -1, [1, 2, 3, 4, 5]], "editable": true, "default":[null]},
+
 
         // output => List of strings in items with Add button
-        "key4": {"type": "[string]", "displayname": "Display Name4", "select": false, "items": ['a', 'b', 'c']},
+        "key6": {"type": int["string"], "displayname": "Display Name6", "displaydesc": "This will come in the hint",
+            "select": [-1, -1, ['a', 'b', 'c', 'd']], "editable": true, "default":[null]},
+
+
 
         // output => List of integers in items with checkboxes (no items are selected at this point in time)
-        "key5": {"type": "[int]", "displayname": "Display Name5", "select": true, "items": [1, 2, 3]},
+        "key7": {"type": ["int"], "displayname": "Display Name7", "displaydesc": "This will come in the hint",
+            "select": [0, 1, [1, 3, 9, 27, 81]],
+            "editable": true, "default":[3]
+        },
+
 
         // output => List of strings in items with checkboxes (no items are selected at this point in time)
-        "key6": {"type": "[string]", "displayname": "Display Name6", "select": true, "items": ['a', 'b', 'c', 'd']},
-    }],
-    ["perm2", "SETTINGS_TAG_2", {"key1": "blah1", "key2": "blah2"}]
+        "key8": {"type": ["string"], "displayname": "Display Name8", "displaydesc": "This will come in the hint",
+            "select": [0, 10000, ['a', 'b', 'c']],
+            "editable": true, "default":["b"]
+        },
+
+
+        // type is object
+        "key5": {
+            "type": {
+                "level2key1": {
+                    "type": "int", "displayname": "Level2 key1", "default": 10, "editable": true
+                },
+                "level2key2": {
+                    "type": "string", "displayname": "Level2 key2", "default": "abc", "editable": true
+                }
+            },
+            "displayname": "Level 2 Keys Array",
+            "default": {"level2key1": 100, "level2key2": 200},
+            "checkfun": {
+                "type": "udf",
+                "func": 'function(inpdataofthisfield, overalldataofthisjson, schemaforthisfield, overallschema)' +
+                '{return {"status":"FAILURE", "errmsg":"I havent yet checked it"}}'
+            }
+        },
+
+
+        // type is [object]
+        "key7": {
+            "type": [{
+                "level2key1": {
+                    "type": "int", "displayname": "Level 2 Key", "default": 10, "editable": true
+                }
+            }],
+            "displayname": "Level 2 Keys Array",
+            "default": [{"level2key1": 100}, {"level2key1": 200}],
+            "checkfun": {
+                "type": "udf",
+                "func": 'function(inpdataofthisfield, overalldataofthisjson, schemaforthisfield, overallschema)' +
+                '{return {"status":"FAILURE", "errmsg":"I havent yet checked it"}}'
+            }
+        }
+    }]
 ];
