100e108d037af5e18783ab2bdd42055322729a6f
smiddela <smiddela@intellicar.in>
2016-09-18 03:41:58 +0530

    added ignition on off filter

--- src/app/components/landingpage/dashboard/map/map.service.js
+++ src/app/components/landingpage/dashboard/map/map.service.js
@@ -125,6 +125,7 @@
                 vehicleData.carbattery = parseFloat(parseFloat(vehicleData.carbattery).toFixed(2));
                 vehicleData.devbattery = parseFloat(parseFloat(vehicleData.devbattery).toFixed(2));
                 vehicleData.ignitionstatus = vehicleData.ignitionstatus ? "Running" : "Stopped";
+                vehicleData.ignitionONOFF = vehicleData.ignitionstatus ? "ig on" : "ig off";
                 vehicleData.mobilistatusStr = vehicleData.mobilistatus ? "Active" : "Immobilized";
                 //vehicleData.active = vehicleData.mobilistatus ? "Active" : "Inactive";
                 vehicleData.timestamp = new Date(vehicleData.timestamp).toString().replace(" GMT+0530 (IST)", "");
