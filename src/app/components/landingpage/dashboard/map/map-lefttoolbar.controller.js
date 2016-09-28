9036e74b8248edf140fa47685d4bfd264977f45d
rinasm <rinasmusthafa@live.com>
2016-09-28 19:10:02 +0530

    Left Bar Icons

--- src/app/components/landingpage/dashboard/map/map-lefttoolbar.controller.js
+++ src/app/components/landingpage/dashboard/map/map-lefttoolbar.controller.js
@@ -29,16 +29,53 @@
 
 
         vm.leftTB = [
-            {'name':'Geofences', 'icon':'fa-globe', 'state':'home.geofence' },
-            // {'name':'City Limits', 'icon':'fa-cab'},
-            // {'name':'Tasks', 'icon':'fa-tasks' },
-            // {'name':'Tags', 'icon':'fa-tag' },
-            // {'name':'User', 'icon':'fa-user' },
-            // {'name':'Settings', 'icon':'fa-gears' }
+            {'name':'Geofences Reports', 'icon':'fa-bar-chart','type':'button','data':
+                {'type':'stateChange','state':'home.geofence'} },
+
+            {'name':'Geofences ', 'icon':'fa-globe', 'type':'button','data':
+                {'type':'function','function':function (active) {
+                    console.log(active);
+                }}
+            },
+
+            {'type':'line'},
+
+            {'name':'City Limits', 'icon':'fa-road', 'type':'button','data':
+                {'type':'function','function':function () {
+                    console.log('City Limits');
+                }}
+            },
+
+            {'name':'Service Center', 'icon':'fa-wrench','type':'button', 'data':
+                {'type':'function','function':function () {
+                    console.log('Service center');
+                }}
+            },
+
+            {'name':'No Go', 'icon':'fa-ban','type':'button', 'data':
+                {'type':'function','function':function () {
+                    console.log('No Go');
+                }}
+            },
+
+            {'name':'Low Battery', 'icon':'fa-battery-quarter' ,'type':'button', 'data':
+                {'type':'function','function':function () {
+                    console.log('Low Battery');
+                }}
+            },
         ];
 
-        vm.buttonClick = function (state) {
-            dialogService.show(state);
+        vm.buttonClick = function (data) {
+            if (data.type == 'stateChange') {
+                dialogService.show(data.state);
+            } else if (data.type == 'function') {
+                if(data.active){
+                    data.active = false;
+                }else{
+                    data.active = true;
+                }
+                data.function(data.active);
+            }
         }
 
 
