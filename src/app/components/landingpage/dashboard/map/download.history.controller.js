e3c3d27b17b4fe6afc20b81e39a8cb76a4230da3
Harsha <harsha.srinivasa@intellicar.in>
2016-12-13 20:36:49 +0530

    show loading bar in download hsitory tab

--- src/app/components/landingpage/dashboard/map/download.history.controller.js
+++ src/app/components/landingpage/dashboard/map/download.history.controller.js
@@ -7,14 +7,17 @@
         .module('uiplatform')
         .controller('DownloadHistoryController', DownloadHistoryController)
 
-    function DownloadHistoryController(dialogService, userService, reportsService, intellicarAPI) {
+    function DownloadHistoryController(dialogService, userService, reportsService,
+                                       intellicarAPI, $timeout, $log) {
 
         var vm = this;
         dialogService.setTab(4);
 
+        vm.showLoading = false;
+
         userService.getMyAssignedGroupsMap({})
             .then(function (data) {
-                console.log(data);
+                // console.log(data);
                 vm.groups = sortArray(data);
                 vm.selectedGroup = vm.groups[0];
             });
@@ -43,54 +46,75 @@
 
         vm.download = function () {
             downloadCSV();
+            vm.showLoading = true;
+            vm.errorMsg = null;
         };
 
         function downloadCSV() {
             reportsService.getGPSLastseen({"grouppath": vm.selectedGroup.assetpath, "recursive": 1})
-                .then(function (resp) {
-                    var dateFormat = 'DD-MM-YYYY HH:mm';
-                    var csvData = resp.data.data;
-                    var csvArray = [];
-
-                    for (var idx in csvData) {
-
-                        var lastcom = moment.unix(csvData[idx].utctime / 1000).format(dateFormat) + " ";
-                        var pullout = 'ATTATCHED';
-                        if (csvData[idx].carb < 1) {
-                            pullout = 'PULLOUT';
-                        }
-                        var mobility = 'ACTIVE';
-                        if (csvData[idx].mobili == 1) {
-                            mobility = 'IMMOBILIZED';
-                        }
-                        var communication = 'Live';
-                        var currentTime = new Date().getTime();
-                        var lastSeenAt = csvData[idx].utctime;
-                        var noCommThreshold = 8 * 3600 * 1000;
-                        if (currentTime - lastSeenAt > noCommThreshold) {
-                            communication = 'No Comm';
-                        }
-                        var gpstime = moment.unix(csvData[idx].gpstime / 1000).format(dateFormat) + " ";
-                        csvArray.push({
-                            "VEHICLE NO": csvData[idx].vehicleno,
-                            "LAST COMM AT": lastcom,
-                            "PULL OUT": pullout,
-                            "MOBILITY": mobility,
-                            "COMMUNICATION": communication,
-                            "GPS TIME": gpstime
-                        })
-                    }
+                .then(vm.handleSuccess, vm.handleFailure);
+        }
 
 
-                    getAddress(csvData, function (address) {
-                        for (var idx in csvArray) {
-                            csvArray[idx]["ADDRESS"] = address[idx][1];
-                            csvArray[idx]["LAT LNG"] = csvData[idx].lat + ', ' + csvData[idx].lat;
-                        }
-                        intellicarAPI.importFileservice.JSONToCSVConvertor(csvArray, "Vehicles History Report", true);
-                    });
+        vm.handleFailure = function (resp ){
+            $log.log(resp.data.msg);
 
-                });
+            if ( resp.data.msg )
+                vm.errorMsg = resp.data.msg;
+            vm.showLoading = false;
+        }
+
+
+        vm.handleSuccess = function (resp) {
+            $log.log(resp.data.msg);
+            if (resp.data.msg)
+                vm.errorMsg = resp.data.msg;
+            var dateFormat = 'DD-MM-YYYY HH:mm';
+            var csvData = resp.data.data;
+            var csvArray = [];
+
+            for (var idx in csvData) {
+
+                var lastcom = moment.unix(csvData[idx].utctime / 1000).format(dateFormat) + " ";
+                var pullout = 'ATTATCHED';
+                if (csvData[idx].carb < 1) {
+                    pullout = 'PULLOUT';
+                }
+                var mobility = 'ACTIVE';
+                if (csvData[idx].mobili == 1) {
+                    mobility = 'IMMOBILIZED';
+                }
+                var communication = 'Live';
+                var currentTime = new Date().getTime();
+                var lastSeenAt = csvData[idx].utctime;
+                var noCommThreshold = 8 * 3600 * 1000;
+                if (currentTime - lastSeenAt > noCommThreshold) {
+                    communication = 'No Comm';
+                }
+                var gpstime = moment.unix(csvData[idx].gpstime / 1000).format(dateFormat) + " ";
+                csvArray.push({
+                    "VEHICLE NO": csvData[idx].vehicleno,
+                    "LAST COMM AT": lastcom,
+                    "PULL OUT": pullout,
+                    "MOBILITY": mobility,
+                    "COMMUNICATION": communication,
+                    "GPS TIME": gpstime
+                })
+            }
+
+
+            getAddress(csvData, function (address) {
+                for (var idx in csvArray) {
+                    csvArray[idx]["ADDRESS"] = address[idx][1];
+                    csvArray[idx]["LAT LNG"] = csvData[idx].lat + ', ' + csvData[idx].lat;
+                }
+                intellicarAPI.importFileservice.JSONToCSVConvertor(csvArray, "Vehicles History Report", true);
+
+                $timeout ( function () {
+                    vm.showLoading = false;
+                }, 1000);
+            });
+
 
             function getAddress(csvData, callback) {
                 var postBody = {data: []};
@@ -104,8 +128,8 @@
                     })
             }
 
-        }
 
+        }
 
     }
 })();
