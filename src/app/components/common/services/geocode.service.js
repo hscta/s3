b473ba2d6933979a2bb7e7b2b1740f00b44fe676
Harsha <harsha.srinivasa@intellicar.in>
2016-12-14 19:34:25 +0530

    geocode service

--- src/app/components/common/services/geocode.service.js
+++ src/app/components/common/services/geocode.service.js
@@ -9,12 +9,31 @@
     angular.module('uiplatform')
         .service('geocodeService', geocodeService);
 
-    function geocodeService($log, requestService) {
+    function geocodeService($log, requestService, $q) {
         var vm = this;
         $log.log("geocodeService");
 
+
+        var handleGetAddress = function(resp) {
+            // $log.log(resp.data.data);
+
+            if (resp.data && resp.data.data && resp.data.data.length) {
+                return $q.resolve(resp.data.data);
+            }
+
+            return $q.reject(resp);
+        };
+
+
+        var handleGetAddressFailure = function (resp) {
+            $log.log(resp);
+            return $q.reject(resp);
+        };
+
+
         vm.getAddress = function(body){
-            return requestService.fireGeoCode('/geocodeint', body);
+            return requestService.fireGeoCode('/geocodeint', body)
+                .then(handleGetAddress, handleGetAddressFailure);
         }
     }
 })();
